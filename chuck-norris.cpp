a9a29faecef1e0bb4c0ecab5bbe8e70f16d0cc2d
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-06-09 18:35:31 +0200

    WIP still in progress

--- chuck-norris.cpp
+++ chuck-norris.cpp
@@ -20,10 +20,28 @@ int main()
     {
       binaryMessage.append( bitset<7>(i).to_string()) ;
     }
-    
+    int consecutiveBit = 0;
+    char previousBit = binaryMessage.front();
     for(auto bit : binaryMessage)
     {
-        
+        if( bit != previousBit)
+        {
+            if( bit == '1')
+            {
+                std::cout << "00 ";
+            }
+            else
+            {
+                std::cout << "0 ";
+            }
+            std::cout << std::string(consecutiveBit, '0');
+            previousBit = bit;
+            consecutiveBit = 0;
+        }
+        else
+        {
+            consecutiveBit++;
+        }
     }
     // Write an action using cout. DON'T FORGET THE "<< endl"
     // To debug: cerr << "Debug messages..." << endl;
